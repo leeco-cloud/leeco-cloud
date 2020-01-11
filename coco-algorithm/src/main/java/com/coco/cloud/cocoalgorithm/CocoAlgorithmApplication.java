c3c0088d0e6fbcb6a7b89b00eac129ebd9d6ac89
liuqiang <liuqiang@ourdocker.cn>
2020-01-11 13:16:36 +0800

    add coco-algorithm

---
+++ coco-algorithm/src/main/java/com/coco/cloud/cocoalgorithm/CocoAlgorithmApplication.java
@@ -0,0 +1,13 @@
+package com.coco.cloud.cocoalgorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocoAlgorithmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CocoAlgorithmApplication.class, args);
+    }
+
+}
