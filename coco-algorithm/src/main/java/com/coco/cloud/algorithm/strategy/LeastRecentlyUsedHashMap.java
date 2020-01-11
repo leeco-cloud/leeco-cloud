c19cad3899979e4c3176604da548e0e6cacb514c
liuqiang <liuqiang@ourdocker.cn>
2020-01-11 17:21:03 +0800

    add coco-algorithm 一致性hash算法

---
+++ coco-algorithm/src/main/java/com/coco/cloud/algorithm/strategy/LeastRecentlyUsedHashMap.java
@@ -0,0 +1,117 @@
+package com.coco.cloud.algorithm.strategy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * LRU(最近最久未使用算法) HashMap方式
+ * @author liuqiang@ourdocker.cn
+ * @version 0.0.1
+ * @date 2020/1/11 17:09
+ */
+public class LeastRecentlyUsedHashMap {
+
+    static class Node {
+        int key;
+        int val;
+        Node prev;
+        Node next;
+    }
+
+    private int capacity;
+
+    /**
+     * 保存链表的头节点和尾节点
+     */
+    private Node first;
+    private Node last;
+
+    private Map<Integer, Node> map;
+
+    private LeastRecentlyUsedHashMap(int capacity) {
+        this.capacity = capacity;
+        map = new HashMap<>(capacity);
+    }
+
+    private int get(int key) {
+        Node node = map.get(key);
+        //为空返回-1
+        if (node == null) {
+            return -1;
+        }
+        moveToHead(node);
+        return node.val;
+    }
+
+    private void moveToHead(Node node) {
+        if (node == first) {
+            return;
+        } else if (node == last) {
+            last.prev.next = null;
+            last = last.prev;
+        } else {
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+        }
+
+        node.prev = first.prev;
+        node.next = first;
+        first.prev = node;
+        first = node;
+    }
+
+    private void put(int key, int value) {
+        Node node = map.get(key);
+
+        if (node == null) {
+            node = new Node();
+            node.key = key;
+            node.val = value;
+
+            if(map.size() == capacity) {
+                removeLast();
+            }
+
+            addToHead(node);
+            map.put(key, node);
+        } else {
+            node.val = value;
+            moveToHead(node);
+        }
+    }
+
+    private void addToHead(Node node) {
+        if (map.isEmpty()) {
+            first = node;
+            last = node;
+        } else {
+            node.next = first;
+            first.prev = node;
+            first = node;
+        }
+    }
+
+    private void removeLast() {
+        map.remove(last.key);
+        Node prevNode = last.prev;
+        if (prevNode != null) {
+            prevNode.next = null;
+            last = prevNode;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return map.keySet().toString();
+    }
+
+    public static void main(String[] args) {
+        LeastRecentlyUsedHashMap cache = new LeastRecentlyUsedHashMap(3);
+        cache.put(1, 1);
+        cache.put(2, 2);
+        cache.put(3, 3);
+        cache.get(1);
+        cache.put(4, 3);
+        System.out.println(cache);
+    }
+}
---
+++ coco-algorithm/src/main/java/com/coco/cloud/algorithm/strategy/LeastRecentlyUsedLinkedHashMap.java
@@ -0,0 +1,44 @@
+package com.coco.cloud.algorithm.strategy;
+
+import java.util.LinkedHashMap;
+
+/**
+ * LRU(最近最久未使用算法) LinkedHashMap方式
+ * @author liuqiang@ourdocker.cn
+ * @version 0.0.1
+ * @date 2020/1/11 17:15
+ */
+public class LeastRecentlyUsedLinkedHashMap<K, V> extends LinkedHashMap<K, V> {
+
+    /**
+     * 设定最大缓存空间 MAX_ENTRIES 为 3
+     */
+    private static final int MAX_ENTRIES = 3;
+
+    /**
+     * 使用LinkedHashMap的构造函数将 accessOrder设置为 true，开启 LRU顺序
+     */
+    private LeastRecentlyUsedLinkedHashMap() {
+        super(MAX_ENTRIES, 0.75f, true);
+    }
+
+    /**
+     * 最后覆盖removeEldestEntry(）方法实现，在节点多于 MAX_ENTRIES 就会将最近最少使用的数据移除。
+     * 因为这个函数默认返回false，不重写的话缓存爆了的时候无法删除最近最久未使用的节点
+     */
+    @Override
+    protected boolean removeEldestEntry(java.util.Map.Entry<K, V> eldest) {
+        //在容量超过最大允许节点数的时候返回true，使得在afterNodeInsertion函数中能执行removeNode()
+        return size() > MAX_ENTRIES;
+    }
+
+    public static void main(String[] args) {
+        LeastRecentlyUsedLinkedHashMap<Integer, Integer> cache = new LeastRecentlyUsedLinkedHashMap<>();
+        cache.put(1, 1);
+        cache.put(2, 2);
+        cache.put(3, 3);
+        cache.get(1);
+        cache.put(4, 4);
+        System.out.println(cache.keySet());
+    }
+}
