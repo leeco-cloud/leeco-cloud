1e43c8b7db8a081de773525849bc5305617a7740
liuqiang <liuqiang@ourdocker.cn>
2020-02-28 18:43:40 +0800

    add coco-design-patterns :singleton

---
+++ coco-design-patterns/src/main/java/com/coco/cloud/design/patterns/singleton/ContainSingleton.java
@@ -0,0 +1,26 @@
+package com.coco.cloud.design.patterns.singleton;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author liuqiang@ourdocker.cn
+ * @version 0.0.1
+ * @date 2020/2/28 18:18
+ */
+public class ContainSingleton {
+
+    private final static Map<String, ContainSingleton> singletonMap = new ConcurrentHashMap<>();
+
+    public static ContainSingleton getInstance(String key) {
+        ContainSingleton containSingleton = singletonMap.get(key);
+        if (containSingleton == null){
+            synchronized (singletonMap){
+                containSingleton = new ContainSingleton();
+                singletonMap.put(key,containSingleton);
+            }
+        }
+        return containSingleton;
+    }
+
+}
