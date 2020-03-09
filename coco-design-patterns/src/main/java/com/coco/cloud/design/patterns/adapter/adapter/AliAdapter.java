5e290d3be46e50cb0dab5f712ef178b343536158
liuqiang <liuqiang@ourdocker.cn>
2020-03-09 19:43:14 +0800

    add coco-design-patterns : adapter

---
+++ coco-design-patterns/src/main/java/com/coco/cloud/design/patterns/adapter/adapter/AliAdapter.java
@@ -0,0 +1,19 @@
+package com.coco.cloud.design.patterns.adapter.adapter;
+
+import com.coco.cloud.design.patterns.adapter.AbstractThirdLogin;
+
+import java.util.Map;
+
+/**
+ * @author liuqiang@ourdocker.cn
+ * @version 0.0.1
+ * @date 2020/3/9 19:32
+ */
+public class AliAdapter extends AbstractThirdLogin {
+
+    @Override
+    public void loginByThird(Map<String,String> param) {
+        login(param.get("ticket"),null);
+    }
+
+}
