8851da85481f1d8a60cdd649e8f8b07fd1fd393a
liuqiang <liuqiang@ourdocker.cn>
2020-05-16 22:04:31 +0800

    add coco-algorithm : 旋转子链表

---
+++ coco-algorithm/src/main/java/com/coco/cloud/leetcode/SolutionLinked.java
@@ -0,0 +1,60 @@
+package com.coco.cloud.leetcode;
+
+/**
+ * 旋转子链表
+ * @author liuqiang@ourdocker.cn
+ * @version 0.0.1
+ * @date 2020/5/16 20:09
+ */
+public class SolutionLinked {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        // 创建空的头节点
+        ListNode tempHead = new ListNode(0);
+        tempHead.next = head;
+        ListNode pre = tempHead;
+        // 开始的时候 尾节点就是头节点
+        ListNode end = tempHead;
+        // 当前不是尾节点
+        while(end.next != null){
+            for(int i = 0;i<k && end != null;i++){
+                end = end.next;
+            }
+            if (end == null){
+                break;
+            }
+            // 断开next连接
+            ListNode tempStart = pre.next;
+            ListNode tempNext = end.next;
+            end.next = null;
+            // 旋转子连接
+            pre.next = reverse(tempStart);
+            tempStart.next = tempNext;
+            pre = tempStart;
+            end = pre;
+        }
+        return tempHead.next;
+    }
+
+    /**
+     * 子链表旋转
+     */
+    private ListNode reverse(ListNode head){
+        ListNode pre = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = nextTemp;
+        }
+        return pre;
+    }
+
+}
---
+++ coco-algorithm/src/main/java/com/coco/cloud/leetcode/SolutionMap.java
@@ -0,0 +1,39 @@
+package com.coco.cloud.leetcode;
+
+import java.util.HashMap;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingDeque;
+
+/**
+ * @author liuqiang@ourdocker.cn
+ * @version 0.0.1
+ * @date 2020/5/15 21:59
+ */
+public class SolutionMap {
+
+    public int subarraySum(int[] nums, int k) {
+        int count = 0, pre = 0;
+        HashMap < Integer, Integer > mp = new HashMap< >();
+        mp.put(0, 1);
+        for (int i = 0; i < nums.length; i++) {
+            pre += nums[i];
+            if (mp.containsKey(pre - k)){
+                count += mp.get(pre - k);
+            }
+            mp.put(pre, mp.getOrDefault(pre, 0) + 1);
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[5];
+        int k = 6;
+        nums[0] = 1;
+        nums[1] = 3;
+        nums[2] = 2;
+        nums[3] = 1;
+        nums[4] = 5;
+        System.out.println(new SolutionMap().subarraySum(nums,k));
+    }
+
+}
