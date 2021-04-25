9fc0d8ab1d34ee790aa1a1d0139f0fa461b8e48e
leeco-cloud <a6265391@qq.com>
2021-04-25 15:44:53 +0800

    add coco-algorithm : 最小栈

---
+++ coco-algorithm/src/main/java/com/coco/cloud/dataStruct/最小堆.java
@@ -0,0 +1,59 @@
+package com.coco.cloud.dataStruct;
+
+import java.util.Stack;
+
+/**
+ * @author liuqiang@ourdocker.cn
+ * @version 0.0.1
+ * @date 2021/4/25 15:37
+ */
+public class 最小堆 {
+
+    private static Stack<Integer> allNumberStack = new Stack<>();
+
+    private static Stack<Integer> minNumberStack = new Stack<>();
+
+    public static void push(Integer number){
+        allNumberStack.push(number);
+        if (!minNumberStack.isEmpty()){
+            if (minNumberStack.peek() >= number){
+                minNumberStack.push(number);
+            }
+        }else{
+            minNumberStack.push(number);
+        }
+    }
+
+    public static Integer pop(){
+        Integer pop = allNumberStack.pop();
+        if (minNumberStack.peek().equals(pop)){
+            minNumberStack.pop();
+        }
+        return pop;
+    }
+
+    public static Integer findMinNumber(){
+        if (minNumberStack.isEmpty()){
+            return null;
+        }
+        return minNumberStack.peek();
+    }
+
+    public static void main(String[] args) {
+        push(3);
+        System.out.println(findMinNumber());
+        pop();
+        push(4);
+        System.out.println(findMinNumber());
+        push(2);
+        System.out.println(findMinNumber());
+        push(1);
+        System.out.println(findMinNumber());
+        pop();
+        push(9);
+        System.out.println(findMinNumber());
+        push(6);
+        System.out.println(findMinNumber());
+    }
+
+}
