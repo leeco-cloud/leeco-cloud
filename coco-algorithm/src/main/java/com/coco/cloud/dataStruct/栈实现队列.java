1fa0fb6f30f37df7495d8302f0a44aec6d8a3431
leeco-cloud <a6265391@qq.com>
2021-04-25 16:17:12 +0800

    add coco-algorithm : 栈实现队列

---
+++ coco-algorithm/src/main/java/com/coco/cloud/dataStruct/栈实现队列.java
@@ -0,0 +1,42 @@
+package com.coco.cloud.dataStruct;
+
+import java.util.Stack;
+
+/**
+ * @author liuqiang@ourdocker.cn
+ * @version 0.0.1
+ * @date 2021/4/25 15:46
+ */
+public class 栈实现队列 {
+
+    public static Stack<Integer> tempStack = new Stack<>();
+
+    public static Stack<Integer> stack = new Stack<>();
+
+    public static void push(Integer number){
+        tempStack.push(number);
+    }
+
+    public static Integer pop(){
+        if (stack.isEmpty()){
+            while(!tempStack.isEmpty()){
+                stack.push(tempStack.pop());
+            }
+        }
+        return stack.isEmpty()? null : stack.pop();
+    }
+
+    public static void main(String[] args) {
+        push(3);
+        push(4);
+        push(2);
+        push(1);
+        push(9);
+        push(6);
+
+        while(!stack.isEmpty() || !tempStack.isEmpty()){
+            System.out.println(pop());
+        }
+    }
+
+}
