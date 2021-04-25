73649f1e64b9e6178112fe88bf0737959a2d0878
leeco-cloud <a6265391@qq.com>
2021-04-25 16:28:52 +0800

    add coco-algorithm : 全排列的下一个正整数

---
+++ coco-algorithm/src/main/java/com/coco/cloud/dataStruct/全排列的下一个数.java
@@ -0,0 +1,30 @@
+package com.coco.cloud.dataStruct;
+
+/**
+ * eg : 12345  =>  12354
+ * @author liuqiang@ourdocker.cn
+ * @version 0.0.1
+ * @date 2021/4/25 16:18
+ */
+public class 全排列的下一个数 {
+
+    private static Integer nextNumber(Integer number){
+        byte[] bytes = String.valueOf(number).getBytes();
+        int index = bytes.length - 1;
+        for (int i = index; i > 0; i--) {
+            byte temp = bytes[i - 1];
+            bytes[i - 1] = bytes[i];
+            bytes[i] = temp;
+            if (Integer.parseInt(new String(bytes)) > number){
+                return Integer.parseInt(new String(bytes));
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Integer number = 13245;
+        System.out.println(nextNumber(number));
+    }
+
+}
